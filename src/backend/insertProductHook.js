3064bb7529b2586c3eed7593ec4c9b88325fa4bf
ERKAM GURESEN <erkamguresen@gmail.com>
2021-07-24 11:47:00 -0700

    insert product id bug is fixed

--- src/backend/insertProductHook.js
+++ src/backend/insertProductHook.js
@@ -1,5 +1,18 @@
 // This function is the webhook's request handler.
 exports = async function (payload, response) {
+  const collection = context.services
+    .get("mongodb-atlas")
+    .db("OnlineShop")
+    .collection("products");
+
+  let productList = await collection.find().toArray();
+
+  const newId =
+    productList.reduce(
+      (accumulator, currentValue) => Math.max(accumulator, currentValue.id),
+      -1
+    ) + 1;
+
   if (payload.body) {
     const body = EJSON.parse(payload.body.text());
     const productsCollection = context.services
@@ -8,7 +21,7 @@ exports = async function (payload, response) {
       .collection("products");
 
     const newProductDoc = {
-      id: body.id,
+      id: newId,
       categoryId: body.categoryId,
       productName: body.productName,
       quantityPerUnit: body.quantityPerUnit,
